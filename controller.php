99aa4cadbd7ddadd839910ee60cf8220b1eb37be
guyasyou <guyasyou@gmail.com>
2018-01-19 23:24:31 +0700

    fix version

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends PackageInstaller
 {
     protected $pkgHandle = 'pure_cookies_notice';
     protected $appVersionRequired = '8.1';
-    protected $pkgVersion = '1.3.1';
+    protected $pkgVersion = '1.3.2';
 
     public function getPackageName()
     {
